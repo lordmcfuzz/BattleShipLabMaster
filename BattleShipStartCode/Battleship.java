c27da42fd808572fc67d60e4ab62bfe98e8c3f9d
vollingerm <lordmcfuzz@gmail.com>
2016-11-07 20:54:02 -0500

    capitalization matters

--- BattleShipStartCode/Battleship.java
+++ BattleShipStartCode/Battleship.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class battleship {
+public class Battleship {
     public static void main (String[] args){
         Board playerOne = new Board();
         playerOne.printBoard();
