2203b9daae4779ce077183833d5ab8f946884f69
vollingerm <lordmcfuzz@gmail.com>
2016-11-07 20:53:23 -0500

    Battleship.java starter code
    includes main with first board print
    includes a crude clearConsole method
    includes a createLine method just in case

---
+++ BattleShipStartCode/Battleship.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class battleship {
+    public static void main (String[] args){
+        Board playerOne = new Board();
+        playerOne.printBoard();
+    }
+    
+    // crude way to clear the screen
+    public final static void clearConsole(){
+        for(int i = 0; i<50; i++) System.out.println();
+    }
+    
+    public static void createLine() {
+        for(int i =0; i<18; i ++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+    
+       
+    
+}
